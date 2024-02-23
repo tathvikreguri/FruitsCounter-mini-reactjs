eceb2ed008e4f6b3c38661e9bb94bc5ed7918211
Tathvik Reguri <83580358+tathvikreguri@users.noreply.github.com>
2024-02-23 09:35:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-import {Component} from 'react'
-
-import './index.css'
-
-class ClickCounter extends Component {
-  state = {count: 0}
-
-  incCount = () => {
-    this.setState(prevState => ({count: prevState.count + 1}))
-  }
-
-  render() {
-    const {count} = this.state
-
-    return (
-      <div className="c1">
-        <h1 className="h1">
-          The Button has been clicked <span className="spn">{count}</span> times
-        </h1>
-        <p className="p1">Click the button to increase the count</p>
-        <button type="button" onClick={this.incCount} className="b1">
-          Click Me!
-        </button>
-      </div>
-    )
-  }
-}
-export default ClickCounter
-************************************
-reactjs application for counting Mangoes
+//reactjs application for counting Mangoes
 -------------------------------------------
-// Write your code here
 import {Component} from 'react'
 
 import './index.css'
